5a044c7b4212ea1728508d9c35bc207a1ca9764b
howyi <howyi.lq@gmail.com>
2019-03-18 17:41:00 +0900

    composer require howyi/conv-laravel

---
+++ config/conv.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'paths' => [
+        'migrations' => 'database/migrations',
+        'schemas'    => 'database/schemas',
+    ],
+    'ignore_tables' => [
+        'migrations'
+    ],
+];
